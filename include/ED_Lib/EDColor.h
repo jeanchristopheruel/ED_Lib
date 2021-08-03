fd1df5393cb5b25983022c162a8b42f2cc37f43b
ruelj2 <ruelj2@gmail.com>
2021-08-03 05:55:56 -0400

    add a fancy structure

--- include/ED_Lib/EDColor.h
+++ include/ED_Lib/EDColor.h
@@ -19,10 +19,10 @@
 * By using this library you are implicitly assumed to have accepted all of the above statements,
 * and accept to cite the following papers:
 *
-* [1] C. Topal and C. Akinlar, “Edge Drawing: A Combined Real-Time Edge and Segment Detector,”
+* [1] C. Topal and C. Akinlar, ï¿½Edge Drawing: A Combined Real-Time Edge and Segment Detector,ï¿½
 *     Journal of Visual Communication and Image Representation, 23(6), 862-872, doi:10.1016/j.jvcir.2012.05.004 (
 *
-* [2] C. Akinlar and C. Topal, “EDPF: A Real-time Parameter-free Edge Segment Detector with a False Detection Con
+* [2] C. Akinlar and C. Topal, ï¿½EDPF: A Real-time Parameter-free Edge Segment Detector with a False Detection Con
 *     International Journal of Pattern Recognition and Artificial Intelligence, 26(1), doi:10.1142/S0218001412550
 *
 * [3] C. Akinlar, C. Topal, "ColorED: Color Edge and Segment Detection by Edge Drawing (ED),"
@@ -32,8 +32,8 @@
 #ifndef  _EDColor_
 #define _EDColor_
 
-#include <opencv2/opencv.hpp>
-
+#include <opencv2/core.hpp>
+namespace ED {
 // Look up table size for fast color space conversion
 #define LUT_SIZE (1024*4096)
 
@@ -48,61 +48,72 @@
 #define MIN_PATH_LEN 10
 
 
-class EDColor {
-public:
-	EDColor(cv::Mat srcImage, int gradThresh = 20, int anchor_thresh = 4, double sigma = 1.5, bool validateSegments=false);
-	cv::Mat getEdgeImage();
-	std::vector<std::vector<cv::Point>> getSegments();
-	int getSegmentNo();
-	
-	int getWidth();
-	int getHeight();
+    class EDColor {
+    public:
+        EDColor(cv::Mat srcImage, int gradThresh = 20, int anchor_thresh = 4, double sigma = 1.5,
+                bool validateSegments = false);
 
-	cv::Mat inputImage;
-private:
-	uchar *L_Img;
-	uchar *a_Img;
-	uchar *b_Img;
+        cv::Mat getEdgeImage();
 
-	uchar *smooth_L;
-	uchar *smooth_a;
-	uchar *smooth_b;
+        std::vector<std::vector<cv::Point>> getSegments();
 
-	uchar *dirImg;
-	short *gradImg;
-	
-	cv::Mat edgeImage;
-	uchar *edgeImg;
+        int getSegmentNo();
 
-	const uchar *blueImg;
-	const uchar *greenImg;
-	const uchar *redImg;
-	
-	int width;
-	int height;
+        int getWidth();
 
-	double divForTestSegment;
-	double *H;
-	int np;
-	int segmentNo;
+        int getHeight();
 
-	std::vector<std::vector<cv::Point>> segments;
+        cv::Mat inputImage;
+    private:
+        uchar *L_Img;
+        uchar *a_Img;
+        uchar *b_Img;
 
-	static double LUT1[LUT_SIZE + 1];
-	static double LUT2[LUT_SIZE + 1];
-	static bool LUT_Initialized;
+        uchar *smooth_L;
+        uchar *smooth_a;
+        uchar *smooth_b;
 
-	void MyRGB2LabFast();
-	void ComputeGradientMapByDiZenzo();
-	void smoothChannel(uchar *src, uchar *smooth, double sigma);
-	void validateEdgeSegments();
-	void testSegment(int i, int index1, int index2);
-	void extractNewSegments();
-	double NFA(double prob, int len);
+        uchar *dirImg;
+        short *gradImg;
 
-	static void fixEdgeSegments(std::vector<std::vector<cv::Point>> map, int noPixels);
+        cv::Mat edgeImage;
+        uchar *edgeImg;
 
-	static void InitColorEDLib();
-};
+        const uchar *blueImg;
+        const uchar *greenImg;
+        const uchar *redImg;
 
+        int width;
+        int height;
+
+        double divForTestSegment;
+        double *H;
+        int np;
+        int segmentNo;
+
+        std::vector<std::vector<cv::Point>> segments;
+
+        static double LUT1[LUT_SIZE + 1];
+        static double LUT2[LUT_SIZE + 1];
+        static bool LUT_Initialized;
+
+        void MyRGB2LabFast();
+
+        void ComputeGradientMapByDiZenzo();
+
+        void smoothChannel(uchar *src, uchar *smooth, double sigma);
+
+        void validateEdgeSegments();
+
+        void testSegment(int i, int index1, int index2);
+
+        void extractNewSegments();
+
+        double NFA(double prob, int len);
+
+        static void fixEdgeSegments(std::vector<std::vector<cv::Point>> map, int noPixels);
+
+        static void InitColorEDLib();
+    };
+}
 #endif // ! _EDColor_
--- include/ED_Lib/EDPF.h
+++ include/ED_Lib/EDPF.h
@@ -17,25 +17,32 @@
 
 #include "ED.h"
 
+namespace ED {
 #define MAX_GRAD_VALUE 128*256
 #define EPSILON 1.0
 
-class EDPF : public ED {
-public:
-	EDPF(cv::Mat srcImage);
-	EDPF(ED obj);
-	EDPF(EDColor obj);
-private:
-	double divForTestSegment;
-	double *H;
-	int np;
-	short *gradImg;
+    class EDPF : public ED {
+    public:
+        EDPF(cv::Mat srcImage);
 
-	void validateEdgeSegments();
-	short *ComputePrewitt3x3(); // differs from base class's prewit function (calculates H)
-	void TestSegment(int i, int index1, int index2);
-	void ExtractNewSegments();
-	double NFA(double prob, int len);		  
-};
+        EDPF(ED obj);
 
+        EDPF(EDColor obj);
+
+    private:
+        double divForTestSegment;
+        double *H;
+        int np;
+        short *gradImg;
+
+        void validateEdgeSegments();
+
+        short *ComputePrewitt3x3(); // differs from base class's prewit function (calculates H)
+        void TestSegment(int i, int index1, int index2);
+
+        void ExtractNewSegments();
+
+        double NFA(double prob, int len);
+    };
+}
 #endif // ! _EDPF_
